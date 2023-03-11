e0ffa1f190f6ed07c1dbbb1cd4647405e61d5f67
private <sjdata@duck.com>
2023-03-10 19:28:54 -0500

    Added enabled state over database

--- chatbot.py
+++ chatbot.py
@@ -6,12 +6,20 @@ import os
 from datetime import datetime
 
 import psycopg2
+from psycopg2 import sql
 import openai
 from dotenv import load_dotenv
 from telegram import Update
 from telegram.ext import ApplicationBuilder, ContextTypes, MessageHandler, CommandHandler, filters
 
+load_dotenv()
 
+conn = psycopg2.connect(
+    host=os.getenv("DATABASE_HOST"),
+    database=os.getenv("DATABASE_NAME"),
+    user=os.getenv("DATABASE_USER"),
+    password=os.getenv("DATABASE_PASSWORD")
+)
 # Constants and configuration values
 OPENAI_MODEL = "gpt-3.5-turbo"
 MAX_TOKENS = 128
@@ -20,14 +28,6 @@ MAX_LOG = 4  # How many messages will be "Remembered", Keep even number
 Alinks = [{'NVMe SSD': 'https://amzn.to/40gnHqz', 'USB Keyboard': 'https://amzn.to/3ZzT0MS',
            'Portable SSD': 'https://amzn.to/3YFPjEe', '3060 GPU': 'https://amzn.to/3JaB2td',
            'usb hub': 'https://amzn.to/3mA1Ewa'}]
-
-global respond_enabled
-respond_enabled = False
-
-
-# Load environment variables from .env file
-def load_environment_variables() -> None:
-    load_dotenv()
 
 
 # Set up OpenAI API key and Telegram bot token
@@ -48,6 +48,16 @@ logging.basicConfig(
 )
 
 
+def get_user_status(conn, user_id):
+    """Get the user's current status from the database."""
+    cur = conn.cursor()
+    query = sql.SQL("SELECT respond_enabled FROM messages WHERE user_id = %s")
+    cur.execute(query, (user_id,))
+    result = cur.fetchone()
+    if result is None:
+        return False
+    return result[0]
+
 def insert_user_message(conn, message, response_message):
     """Insert user message into PostgreSQL database."""
     cur = conn.cursor()
@@ -65,27 +75,66 @@ def insert_user_message(conn, message, response_message):
     cur.execute(insert_query, (user_id, username, message_text, bot_response, timestamp))
 
     conn.commit()
+
+
 async def start(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Send a welcome message to the user."""
-    global respond_enabled
+    user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
+    name = update.message.from_user.first_name
 
-    respond_enabled = not respond_enabled
+    current_status = get_user_status(conn, user_id)
+    new_status = not current_status
 
-    status = "on" if respond_enabled else "off"
+    with conn:
+        with conn.cursor() as cur:
+            query = sql.SQL("UPDATE messages SET respond_enabled = %s WHERE user_id = %s")
+            cur.execute(query, (new_status, user_id))
 
-    name: str = update.message.from_user.first_name
-    chat_id: int = update.message.chat_id
+    status = "on" if new_status else "off"
     welcome_message = f"Hi {name}! I'm a bot designed to answer questions. I will now respond to each message, " \
-                      f"stop by sending /start again. status is {status}"
+                      f"stop by sending /stop. status is {status}"
     await context.bot.send_message(chat_id=chat_id, text=welcome_message)
 
 
+async def stop(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    """Send a goodbye message to the user."""
+    user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
+    name = update.message.from_user.first_name
+
+    current_status = get_user_status(conn, user_id)
+    new_status = False
+
+    with conn:
+        with conn.cursor() as cur:
+            query = sql.SQL("UPDATE messages SET respond_enabled = %s WHERE user_id = %s")
+            cur.execute(query, (new_status, user_id))
+
+    goodbye_message = f"Bye {name}! I will no longer respond to your messages. " \
+                      f"Send /start to start the bot again."
+    await context.bot.send_message(chat_id=chat_id, text=goodbye_message)
+
 async def respond(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     """Generate a response to a user message."""
-    global respond_enabled
+    user_id = update.message.from_user.id
+    chat_id = update.message.chat_id
+    message_text = update.message.text
 
-    if not respond_enabled:
-        return
+    with psycopg2.connect(
+            host=os.getenv("DATABASE_HOST"),
+            database=os.getenv("DATABASE_NAME"),
+            user=os.getenv("DATABASE_USER"),
+            password=os.getenv("DATABASE_PASSWORD")
+    ) as conn:
+        with conn.cursor() as cur:
+            query = sql.SQL("SELECT respond_enabled FROM messages WHERE user_id = %s")
+            cur.execute(query, (user_id,))
+            row = cur.fetchone()
+
+            if row is None or not row[0]:
+                # respond_enabled is False for this user
+                return
 
     name: str = update.message.from_user.first_name
     chat_id: int = update.message.chat_id
@@ -121,19 +170,13 @@ async def respond(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
     messages_dict.setdefault(chat_id, []).append(response_message_obj)
 
     # Insert the user message and bot response into the database
-    with psycopg2.connect(
-        host=os.getenv("DATABASE_HOST"),
-        database=os.getenv("DATABASE_NAME"),
-        user=os.getenv("DATABASE_USER"),
-        password=os.getenv("DATABASE_PASSWORD")
-    ) as conn:
+    with conn:
         insert_user_message(conn, update.message, response_message)
     # Send the response to the user
     await context.bot.send_message(chat_id=chat_id, text=response_message)
 
 
 def main() -> None:
-    load_environment_variables()
     configure()
 
     # Create the Telegram bot application
@@ -142,6 +185,7 @@ def main() -> None:
     # Add handlers for commands and messages
     response_handler = MessageHandler(filters.TEXT, respond)
     application.add_handler(CommandHandler("start", start))
+    application.add_handler(CommandHandler("stop", stop))
     application.add_handler(response_handler)
 
     # Start the bot
